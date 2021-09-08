5550bb166c92506532b4ecb91f1396b26802b683
Lukas <Lukas.v.hulst@gmail.com>
2021-09-08 09:55:52 +0200

    Heb feestlunch.py en spaalhaldag.py gemaakt

---
+++ feestlunch.py
@@ -0,0 +1,11 @@
+#prijzen van producten
+prijzcroissant = 0.39
+prijzstokbrood = 2.78
+waardekorting  = 0.50
+
+#hoeveelheid producten
+aantcroissant = 17
+aantstokbrood = 2
+aantkorting   = 3
+
+print(aantcroissant * prijzcroissant + aantstokbrood * prijzstokbrood - aantkorting * waardekorting)
---
+++ speelhaldag.py
@@ -0,0 +1,7 @@
+personen = 4
+prijskaart = 7.45
+VRprijs = 0.37 #per 5 minuten
+VRtijdpersoon = 45
+VRtotaaltijd = personen * VRtijdpersoon
+
+print(personen * prijskaart + VRtotaaltijd / 5 * VRprijs)
